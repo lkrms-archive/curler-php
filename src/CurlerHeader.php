d121199db6baab9ace78c2423f8b6e1f82a8fa2a
Luke Arms <luke@linacreative.com>
2019-06-26 12:40:59 +1000

    Initial commit (untested in this form)

---
+++ src/CurlerHeader.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Curler;
+
+class CurlerHeader
+{
+    private $Headers = array('user-agent' => 'User-Agent:Curler PHP (https://github.com/lkrms/curler-php)');
+
+    public function SetHeader($name, $value)
+    {
+        $name   = trim($name);
+        $value  = trim($value);
+
+        // HTTP headers are case-insensitive, so make sure we don't end up with duplicates
+        $this->Headers[strtolower($name)] = "{$name}:{$value}";
+    }
+
+    public function GetHeaders()
+    {
+        return array_values($this->Headers);
+    }
+}
